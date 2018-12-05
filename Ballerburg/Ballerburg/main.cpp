93e1a52f9b5ac28926808ebbb2fda16e5ab8af92
CHRINIMUE <chrinimue@posteo.de>
2018-12-05 15:04:43 +0100

    Optimize main() and game activity

--- Ballerburg/Ballerburg/main.cpp
+++ Ballerburg/Ballerburg/main.cpp
@@ -12,10 +12,11 @@
 #define VERSION		"V0.1"	
 #define WINHEIGHT	800	
 #define WINWIDTH	1200	
+#define GROUND_HEIGHT WINHEIGHT - (WINHEIGHT / 8)
 #define ROCK_POINTS	50				// Amount of points between rock left - rock middle / rock middle - rock left
 
 // Canon settings
-#define MAX_CANON_HITS 10
+#define MAX_CANON_HITS 20
 #define CANON_BALL_RADIUS 5
 
 // Set colors
@@ -24,7 +25,7 @@
 #define COLOR_ROCK 200, 200, 200
 #define COLOR_CASTLE 0, 0, 255
 
-#define DEBUG 18
+//#define DEBUG 18
 
 
 using namespace cv;
@@ -72,16 +73,24 @@ int CalcDistance(int ax, int ay, int bx, int by, int counter, int amountPoints, 
 }
 
 // Draw the Rock
-void DrawRock(Mat frame, int groundHeight) {
-
+void DrawRock(Mat frame) {
+	// Main Variables
+	int groundHeight = GROUND_HEIGHT;	// generate the bottom ground
 	int rockHeight = rand() % ((groundHeight - 100) - (winHeight / 8)) + (winHeight / 8);			// generate the height of the rock
-	int rockLeft = rand() % (WINWIDTH / 3 - 200) + 200;											// generate start of the rock in 1/3 and 150px away from the border
+	int rockLeft = rand() % (WINWIDTH / 3 - 200) + 200;												// generate start of the rock in 1/3 and 150px away from the border
 	int rockRight = rand() % (WINWIDTH - ((WINWIDTH / 3 * 2) + 200)) + ((WINWIDTH / 3 * 2) - 1);	// generate end of the rock in 3/3 and 150px away from the border
 	int rockWidth = rockRight - rockLeft;			// Calc the width (X) of the rock
 	int rockMiddle = (rockWidth / 2) + rockLeft;	// Calc the middle x cordinate of the rock
 	int yDistancePerPoint;							// Max size between two Y points
 	int xDistancePerPoint;							// Max size between two X points
-	int leftPoints = 0;									// Amount of Points set left from the middle
+	int leftPoints = 0;								// Amount of Points set left from the middle
+
+	// draw the ground
+	for (int y = winHeight; y > groundHeight; y--) {
+		for (int x = 0; x < winWidth; x++) {
+			rectangle(frame, Rect(x, y, 1, 1), Scalar(COLOR_ROCK), 1, 8, 0);
+		}
+	}
 
 	// Create the Rock
 	Point rock_points[1][500];
@@ -151,8 +160,8 @@ void DrawRock(Mat frame, int groundHeight) {
 	#endif	
 }
 
+// Draw Castle in Mat, X Position, Y Position and turn the caste if needed
 void DrawCastle(Mat frame, int xPosition, int yPosition, bool turn) {
-
 	Point castle[1][100]; // Init castle points
 	int counter = 0;
 
@@ -229,100 +238,52 @@ void DrawCastle(Mat frame, int xPosition, int yPosition, bool turn) {
 	2:	Hit Rock
 	*****************************************/
 int check_collistion(int x, int y, Mat frame) {
+	int checkResult = -1;
 	double epsilon = M_PI / 8.0;
 	for (double alpha = 0.0; alpha <= 2 * M_PI; alpha += epsilon) {
-		double xPositionRadius = CANON_BALL_RADIUS * cos(alpha);
-		double yPositionRadius = CANON_BALL_RADIUS * sin(alpha);
-				
-		frame.at<Vec3b>((int)(y + yPositionRadius), (int)(x + xPositionRadius)) = Vec3b(0, 0, 0);
-		// Check hit for every point
+		double xPositionRadius = CANON_BALL_RADIUS * cos(alpha); // X offset of the circle
+		double yPositionRadius = CANON_BALL_RADIUS * sin(alpha); // Y offset of the circle
+
+		// Check hit for every point of the circle hitbox
 		if (frame.at<Vec3b>((int)(y + yPositionRadius), (int)(x + xPositionRadius)) == Vec3b(COLOR_CASTLE)) {
 			// TODO: Add explosion
-			#ifdef DEBUG
-			printf("Hit TRUE: Castle\n");
-			frame.at<Vec3b>((int)(y + yPositionRadius), (int)(x + xPositionRadius)) = Vec3b(0, 0, 0);
-			imshow(windowName, frame);
-			waitKey(10);
-			#endif		
-			return 1;
+			checkResult = 1; 
 		}
 		else if (frame.at<Vec3b>((int)(y + yPositionRadius + 1), (int)(x + xPositionRadius + 1)) == Vec3b(COLOR_ROCK)) {
 			// TODO: Add explosion
-			#ifdef DEBUG
-			printf("Hit TRUE: Rock\n");
-			frame.at<Vec3b>((int)(y + yPositionRadius), (int)(x + xPositionRadius)) = Vec3b(0, 0, 0);
-			imshow(windowName, frame);
-			waitKey(10);
-			#endif
-			return 2;
-		}
-		else {
-			#ifdef DEBUG
-			printf("Hit FALSE\n");
-			#endif
+			checkResult = 2;
 		}
 	}
 
-	#ifdef DEBUG
+	#ifdef DEBUG // Only Debug stuff
 	printf("Middle Hit Check Position: %i, %i\n", x, y);
+	if (checkResult != -1) {
+		printf("Hit TRUE\n");
+		circle(frame, Point(x, y), CANON_BALL_RADIUS, Scalar(0, 0, 0), CV_FILLED, 8, 0);
+		imshow(windowName, frame);
+		waitKey(10);
+	}
+	else {
+		printf("Hit FALSE\n");
+	}
 	#endif
 
-	return -1;
+	return checkResult;
 }
 
-int main() {
-	//Init
-	#ifdef DEBUG
-	srand(DEBUG);
-	#else
-	srand(time(NULL));
-	#endif
-
-	// Main Variables
-	int groundHeight	= winHeight - (winHeight / 8);		// generate the bottom ground
-	int xPosition		= 20;								// X Position of castle
-	int yPosition		= 700;								// Y Position of castle
-
-	Mat frame(winHeight, winWidth, CV_8UC3, Scalar(COLOR_BACKGROUND));
-
-	// draw the ground
-	for (int y = winHeight; y > groundHeight; y--) {
-		for (int x = 0; x < winWidth; x++) {
-			rectangle(frame, Rect(x, y, 1, 1), Scalar(COLOR_ROCK), 1, 8, 0);
-		}
-	}
-
-	// draw the rock
-	DrawRock(frame, groundHeight);
-
-	// Draw two castle
-	DrawCastle(frame, 20, groundHeight, false);
-	DrawCastle(frame, WINWIDTH - 20, groundHeight, true);
-
-	// Der blaue Komenet
-	int blauerkomet = rand() % 1000000;
-	if (blauerkomet == 10) {
-		for (int i = 0; i < WINWIDTH + 30; i++) {
-			circle(frame, Point(i, 70), 10, Scalar(255, 0, 0), CV_FILLED, 8, 0);
-			imshow(windowName, frame);
-			waitKey(10);
-			circle(frame, Point(i - 30, 70), 10, Scalar(COLOR_BACKGROUND), CV_FILLED, 8, 0);
-		}
-	}
-
-	imshow(windowName, frame);
-
-	// Start Position of Canon Ball
+// Draw and calc the canon ball in Mat
+void ShootCanonBall(Mat frame) {
+	// Position of Canon (Start of flying Ball)
 	int xPos = 170;
 	int yPos = 600;
 
 	// Draw the cannonball
 	int hitCounter = 0;
-	for (int x = xPos; x < WINWIDTH; x++) {		
+	for (int x = xPos; x < WINWIDTH; x++) {
 		int y = (int)(0.001 * pow((x - xPos), 2) - (x - xPos) + yPos);
 
-		int checkHitResult = check_collistion(x, y, frame);
-		if ((checkHitResult == 1) || (checkHitResult == 2)) {
+		int hitResult = check_collistion(x, y, frame);
+		if ((hitResult == 1) || (hitResult == 2)) {
 			hitCounter++;
 			if (hitCounter >= MAX_CANON_HITS) {
 				break;
@@ -339,9 +300,53 @@ int main() {
 		printf("Canon Ball Position %i, %i\n", x, y);
 		#endif
 	}
-	
-	waitKey(0);
+}
 
+void InitGame(Mat frame) {
+	int groundHeight = GROUND_HEIGHT;
+	// draw the rock
+	DrawRock(frame);
+
+	// Draw two castle
+	DrawCastle(frame, 20, groundHeight, false);
+	DrawCastle(frame, WINWIDTH - 20, groundHeight, true);
+}
+
+int main() {
+	//Init
+	#ifdef DEBUG
+	srand(DEBUG);
+	#else
+	srand(time(NULL));
+	#endif
+
+	// Init the playground frame
+	Mat frame(winHeight, winWidth, CV_8UC3, Scalar(COLOR_BACKGROUND));
+
+	// Init the playground
+	InitGame(frame);
+
+	// Der blaue Komenet TODO: Move in extra function
+	int blauerkomet = rand() % 1000000;
+	if (blauerkomet == 10) {
+		for (int i = 0; i < WINWIDTH + 30; i++) {
+			circle(frame, Point(i, 70), 10, Scalar(255, 0, 0), CV_FILLED, 8, 0);
+			imshow(windowName, frame);
+			waitKey(10);
+			circle(frame, Point(i - 30, 70), 10, Scalar(COLOR_BACKGROUND), CV_FILLED, 8, 0);
+		}
+	}
+
+	bool play = true;
+	int round = 1;
+	do {
+		ShootCanonBall(frame);
+		round++;
+	} while (play);
+
+	// Start Position of Canon Ball
+	waitKey(0);
+	
 	// Close the Window
 	destroyWindow(windowName);
 }
